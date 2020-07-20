81743106e4ebc9b465348fb7f6bd1c5aff4565b0
EnricMateu <xaxeu@hotmail.com>
2020-07-20 15:18:39 +0200

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<head>
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li class="{{ request()->routeIs('home') ? 'active': ''}}"><a href="/">Home</a></li>
+            <li class="{{ request()->routeIs('about') ? 'active': ''}}"><a href="/about">About</a></li>
+            <li class="{{ request()->routeIs('portfolio') ? 'active': ''}}"><a href="/portfolio">Portfolio</a></li>
+            <li class="{{ request()->routeIs('contact') ? 'active': ''}}"><a href="/contact">Contact</a></li>
+        </ul>
+    </nav>
+</body>
+
+@yield('content')
+
+</html>
