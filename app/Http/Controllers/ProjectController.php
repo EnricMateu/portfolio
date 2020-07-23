cf62ae1ca50909ac86cdc14a0bc9d9596e7e401e
EnricMateu <xaxeu@hotmail.com>
2020-07-23 13:34:45 +0200

    Project edit form with validation

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CreateProjectRequest;
+use App\Http\Requests\SaveProjectRequest;
 use App\Project;
 use Illuminate\Http\Request;
 
@@ -22,7 +22,7 @@ class ProjectController extends Controller
     }
 
    
-    public function store(CreateProjectRequest $request)
+    public function store(SaveProjectRequest $request)
     {
 
         Project::create( $request->validated() );
@@ -39,15 +39,19 @@ class ProjectController extends Controller
     }
 
     
-    public function edit($id)
+    public function edit(Project $project)
     {
-        //
+        return view('projects.edit', [
+            'project' => $project
+        ]);
     }
 
     
-    public function update(Request $request, $id)
+    public function update(Project $project, SaveProjectRequest $request)
     {
-        //
+        $project->update( $request->validated() );
+
+        return redirect()->route('projects.show', $project);
     }
 
    
--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Crear proyecto')
 
 @section('content')
-    <h1>Crear nuevo proyecto</h1>
+    <h1>Editar proyecto</h1>
 
     @if($errors->any())
         <ul>
@@ -13,24 +13,25 @@
         </ul>
     @endif
 
-    <form method="POST" action="{{ route('projects.store') }}">
+    <form method="POST" action="{{ route('projects.update', $project) }}">
         @csrf
+        @method('PATCH')
         <label>
             Titulo del proyecto <br>
-            <input type="text" name="title" value="{{ old('title') }}" >
+            <input type="text" name="title" value="{{ old('title', $project->title) }}" >
         </label>
         <br>
         <label>
             URL del proyecto <br>
-            <input type="text" name="url" value="{{ old('url') }}">
+            <input type="text" name="url" value="{{ old('url', $project->url) }}">
         </label>
         <br>
         <label>
             Descripcion del proyecto <br>
-            <textarea name="description">"{{ old('description') }}"</textarea>
+            <textarea name="description">{{ old('description', $project->description) }}</textarea>
         </label>
         <br>
-        <button>Guardar</button>
+        <button>Actualizar</button>
     </form>
 
 @endsection
