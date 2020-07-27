01429356ba088eaa7bcfa04005881f8007d110e2
EnricMateu <xaxeu@hotmail.com>
2020-07-27 17:21:38 +0200

    contact form styled

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,18 +3,57 @@
 @section('title', 'Contact')
 
 @section('content')
-    <h1>@lang('Contact')</h1>
 
-    <form method="POST" action="{{ route('contact') }}">
-        @csrf
-        <input name="name" placeholder="Nombre..." value="{{ old('name') }}"> <br>
-        {!! $errors->first('name','<small>:message</small><br>') !!} 
-        <input type="email" name="email" placeholder="Email..." value="{{ old('email') }}"><br>
-        {!! $errors->first('email', '<small>:message</small><br>') !!} 
-        <input name="subject" placeholder="Asunto..." value="{{ old('subject') }}"><br>
-        {!! $errors->first('subject', '<small>:message</small><br>') !!} 
-        <textarea name="content" placeholder="Mensaje...">{{ old('content') }}</textarea><br>
-        {!! $errors->first('content', '<small>:message</small><br>') !!} 
-        <button>@lang('Send')</button>
-    </form>
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-sm-10 col-lg-6 mx-auto"> 
+            <form class="bg-white shadow rounded py-3 px-4" method="POST" action="{{ route('contact') }}">
+                @csrf
+                <h1 class="display-4" >@lang('Contact')</h1>
+                <div class="form-group">
+                    <label for="name"></label>
+                    <input class="form-control bg-light shadow-sm 
+                    @error('name') is-invalid @else border-0 @enderror" id="name" name="name" placeholder="Nombre..." value="{{ old('name') }}">
+                    @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="email"></label>
+                    <input class="form-control bg-light shadow-sm 
+                    @error('name') is-invalid @else border-0 @enderror" type="email" id="email" name="email" placeholder="Email..." value="{{ old('email') }}">
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="subject"></label>
+                    <input class="form-control bg-light shadow-sm 
+                    @error('name') is-invalid @else border-0 @enderror" id="subject" name="subject" placeholder="Asunto..." value="{{ old('subject') }}">
+                    @error('subject')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="content"></label>
+                    <textarea class="form-control bg-light shadow-sm 
+                    @error('name') is-invalid @else border-0 @enderror" id="content" name="content" placeholder="Mensaje...">
+                    {{ old('content') }}</textarea>
+                    @error('content')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <button class="btn btn-primary btn-lg btn-block">@lang('Send')</button>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection('content')
