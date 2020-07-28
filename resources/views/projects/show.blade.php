db15579173d8dbfb06799155e2f8ecb75cb6ac3d
EnricMateu <xaxeu@hotmail.com>
2020-07-28 12:13:32 +0200

    project image added

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,10 +4,14 @@
 
 @section('content')
 <div class="container">
-    <div class="bg-white p-5 shadow rounded"> 
+    <div class="row">   
+    <div class="bg-white p-5 shadow rounded col-lg-6"> 
         <h1>{{ $project->title }}</h1>
         <p class="text-secondary">{{ $project->description }}</p>
         <p class="text-black-50">{{ $project->created_at->diffForHumans() }}</p>
+        <div class="col-12 mb-4 col-lg-6">
+                <img class="img-fluid" src="/img/project.svg" alt="Desarrollo Web">
+        </div>
         <div class="d-flex justify-content-between align-items-center">
             <a href="{{ route('projects.index') }}">
                 Regresar
