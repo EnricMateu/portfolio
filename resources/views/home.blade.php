9a208257f73aa575ddaa210d7e0ab54ba064c57d
EnricMateu <xaxeu@hotmail.com>
2020-07-28 11:36:35 +0200

    Home front

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,25 @@
 @section('title', 'Home')
 
 @section('content')
-    <h1>@lang('Home')</h1>
-    @auth
-        {{ auth()->user()->name }}
-    @endauth
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-lg-6">
+            <h1 class="display-4 text-primary">Desarrollo Web</h1>
+            <p class="lead text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Obcaecati doloremque, aliquid adipisci officiis sint nesciunt consectetur dolorem 
+                qui nam magni vel dignissimos autem ipsa eius debitis ex quia reiciendis natus.
+            </p>
+            <a class="btn btn-lg btn-block btn-primary" href="{{ route('contact') }}">
+                Contactame
+            </a>
+            <a class="btn btn-lg btn-block btn-outline-primary" href="{{ route('projects.index') }}">
+                Portafolio
+            </a>
+        </div>
+        <div class="col-12 mb-4 col-lg-6">
+                <img class="img-fluid" src="/img/home.svg" alt="Desarrollo Web">
+        </div>
+    </div>
+</div>
+
 @endsection('content')
